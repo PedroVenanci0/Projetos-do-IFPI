500e3536750ec242941946e8217a4727ba1d846a
PedroVenanci0 <contato.pedrovenancio.dev@gmail.com>
2025-01-30 12:04:06 -0300

    feat: Atv de algoritmos de ordenacao em C

---
+++ Estrutura de Dados/Tarefa_Busca/algoritmosOrdenação.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+void print_array(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main() {
+    int arr1[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr2[] = {64, 34, 25, 12, 22, 11, 90};
+    int arr3[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr1) / sizeof(arr1[0]);
+
+    // Bubble Sort
+    // bubble_sort(arr1, n);
+    // printf("\nBubble Sort: ");
+    // print_array(arr1, n);
+
+    // Insertion Sort
+    // insertion_sort(arr2, n);
+    // printf("\nInsertion Sort: ");
+    // print_array(arr2, n);
+
+    // Selection Sort
+    selection_sort(arr3, n);
+    printf("\nSelection Sort: ");
+    print_array(arr3, n);
+
+    return 0;
+}
